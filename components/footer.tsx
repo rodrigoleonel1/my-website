dba7118e5092c7f4349c5c1b0e6727721c86f6cb
Rodrigo <rodricsxd@gmail.com>
2024-01-16 17:29:06 -0300

    update social links

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,11 +5,26 @@ export default function Footer() {
   return (
     <footer className="flex flex-col place-items-center justify-center mt-12 pb-6">
       <article className="py-2 gap-4 flex text-emerald-400">
-        <GithubIcon
-          size={32}
-          className="cursor-pointer hover:text-black/80 dark:hover:text-white/80"
-        />
-        <LinkedinIcon size={32} className="cursor-pointer hover:text-sky-600" />
+        <a
+          href="https://github.com/rodrigoleonel1"
+          target={"_blank"}
+          rel={"noreferrer"}
+        >
+          <GithubIcon
+            size={32}
+            className="cursor-pointer hover:text-black/80 dark:hover:text-white/80"
+          />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/rodrigoalarc%C3%B3n/"
+          target={"_blank"}
+          rel={"noreferrer"}
+        >
+          <LinkedinIcon
+            size={32}
+            className="cursor-pointer hover:text-sky-600"
+          />
+        </a>
         <MailClipboard
           text={{ contact: "rodricsxd@gmail.com", type: "Email" }}
         />
