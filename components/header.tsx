a3648f275942138111ef36fe66b249f99f0b47ac
Rodrigo Alarcón <rodricsxd@gmail.com>
2025-07-29 12:17:44 -0300

    reset project

--- components/header.tsx
+++
@@ -1,9 +0,0 @@
-import ThemeToggle from "./theme-toggle";
-
-export default function Header() {
-  return (
-    <header className="max-w-3xl mx-auto p-4 flex items-center justify-end">
-      <ThemeToggle />
-    </header>
-  );
-}
